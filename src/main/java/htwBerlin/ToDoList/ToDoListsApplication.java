e256acd0f5084cc5fc076e378f7781f0cd7649a6
Al-SayaghiNasr <108007272+nasrwihtw@users.noreply.github.com>
2023-10-31 16:40:45 +0100

    init

---
+++ src/main/java/htwBerlin/ToDoList/ToDoListsApplication.java
@@ -0,0 +1,13 @@
+package htwBerlin.ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwBerlin/ToDoList/ToDoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package htwBerlin.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
